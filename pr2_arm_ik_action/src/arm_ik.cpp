4d1c21a59992a2edd9476f115fc80d6dab17187a
mwise <mwise@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-04-13 22:55:41 +0000

    moving to using the latest robot model initParam method

--- pr2_arm_ik_action/src/arm_ik.cpp
+++ pr2_arm_ik_action/src/arm_ik.cpp
@@ -1,35 +1,35 @@
-/*********************************************************************       
- * Software License Agreement (BSD License)                                  
- *                                                                           
- *  Copyright (c) 2008, Willow Garage, Inc.                                  
- *  All rights reserved.                                                     
- *                                                                           
- *  Redistribution and use in source and binary forms, with or without       
- *  modification, are permitted provided that the following conditions       
- *  are met:                                                                 
- *                                                                           
- *   * Redistributions of source code must retain the above copyright        
- *     notice, this list of conditions and the following disclaimer.         
- *   * Redistributions in binary form must reproduce the above                
- *     copyright notice, this list of conditions and the following           
- *     disclaimer in the documentation and/or other materials provided       
- *     with the distribution.                                                
- *   * Neither the name of Willow Garage nor the names of its                
- *     contributors may be used to endorse or promote products derived       
- *     from this software without specific prior written permission.         
- *                                                                           
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS        
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE           
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,      
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,     
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;         
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER         
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT       
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN        
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          
- *  POSSIBILITY OF SUCH DAMAGE.                                              
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
  *
  * Author: Melonee Wise
  *********************************************************************/
@@ -60,56 +60,42 @@ public:
     as_(name),
     generator_(2.5, 2.0, dimension_)
   {
-    //register the goal and feeback callbacks                                                            
+    //register the goal and feeback callbacks
     as_.registerGoalCallback(boost::bind(&PR2ArmIKAction::goalCB, this));
     as_.registerPreemptCallback(boost::bind(&PR2ArmIKAction::preemptCB, this));
 
-    // Load Robot Model                                                                                                            
-    std::string urdf_xml, full_urdf_xml, xml_string;
-    nh_.param("urdf_xml", urdf_xml, std::string("robot_description"));
-    nh_.searchParam(urdf_xml, full_urdf_xml);
-    TiXmlDocument xml;
-    ROS_DEBUG("Reading xml file from parameter server\n");
-    std::string result;
-    if (nh_.getParam(full_urdf_xml, result))
-      xml.Parse(result.c_str());
-    else
-      {
-	ROS_FATAL("Could not load the xml from parameter server: %s\n", urdf_xml.c_str());
-	ros::shutdown();
-	exit(1);
-      }
-    xml_string = result;
-    TiXmlElement *root_element = xml.RootElement();
-    TiXmlElement *root = xml.FirstChildElement("robot");
-    if (!root || !root_element)
-      {
-	ROS_FATAL("Could not parse the xml from %s\n", urdf_xml.c_str());
-	ros::shutdown();
-	exit(1);
-      }
-    robot_model.initXml(root);
+    // Load Robot Model
+    ROS_DEBUG("Loading robot model");
+    robot_model.initParam(std::string("robot_description"));
 
-    // Which arm?                                                                                                                  
+    // Get Parameters 
     nh_.param<std::string>("arm", arm_, "r");
     nh_.getParam("arm_controller", arm_controller_ );
+    nh_.param("free_angle", free_angle_, 2);
+    nh_.param("search_discretization", search_discretization_, 0.01);
+    nh_.param("ik_timeout", timeout_, 5.0);
+    nh_.param("max_velocity", max_velocity_, 0.5);
     root_name_ = "torso_lift_link";
     tip_name_ = arm_ + "_wrist_roll_link";
 
 
     ROS_DEBUG("Loading KDL chain");
-    if(!pr2_arm_kinematics::getKDLChain(xml_string, root_name_, tip_name_, kdl_chain_))
-      {
-	ROS_ERROR("Could not load kdl chain");
-	ros::shutdown();
-	exit(1);
-      }
-    // Init IK                                                                                                                                              
-    nh_.param("free_angle", free_angle_, 2);
-    nh_.param("search_discretization", search_discretization_, 0.01);
-    nh_.param("ik_timeout", timeout_, 5.0);
-    nh_.param("max_velocity", max_velocity_, 0.5);
-    ROS_INFO("Starting arm ik action with search_discretization %f, ik_timeout %f and max_velocity %f", search_discretization_,timeout_,max_velocity_);
+    KDL::Tree tree;
+    if (!kdl_parser::treeFromUrdfModel(robot_model, tree))
+    {
+      ROS_ERROR("Could not initialize tree object");
+      ros::shutdown();
+      exit(1);
+    }
+    if (!tree.getChain(root_name_, tip_name_, kdl_chain_))
+    {
+      ROS_ERROR("Could not initialize chain object");
+      ros::shutdown();
+      exit(1);
+    }
+
+    // Init IK
+    ROS_INFO("Starting with search_discretization %f, ik_timeout %f and max_velocity %f", search_discretization_,timeout_,max_velocity_);
     pr2_arm_ik_solver_.reset(new pr2_arm_kinematics::PR2ArmIKSolver(robot_model, root_name_, tip_name_, search_discretization_, free_angle_));
 
     if(!pr2_arm_ik_solver_->active_)
@@ -119,7 +105,7 @@ public:
 	exit(1);
       }
 
-    // Init pose suggestion              
+    // Init pose suggestion
     jnt_pos_suggestion_.resize(dimension_);
 
     ros::NodeHandle nh_toplevel;
@@ -133,7 +119,7 @@ public:
       if(counter > 3)
       {
         ROS_ERROR("%s: Aborted: joint_trajectory_action action server took too long to come up", action_name_.c_str());
-        //set the action state to aborted                
+        //set the action state to aborted
         as_.setAborted(result_);
         return;
       }
@@ -150,26 +136,26 @@ public:
   void preemptCB()
   {
     ROS_INFO("%s: Preempt", action_name_.c_str());
-    // set the action state to preempted                                                                 
+    // set the action state to preempted
     as_.setPreempted();
   }
-  
+
   void goalCB()
   {
-    // accept the new goal                                                      
+    // accept the new goal
     pr2_common_action_msgs::PR2ArmIKGoal goal = *as_.acceptNewGoal();
     ROS_INFO("%s: Accepted Goal", action_name_.c_str() );
-     
-    //Try to transform the pose to the root link frame                                                                                                      
+
+    //Try to transform the pose to the root link frame
     bool ret1 = false;
     tf::Stamped<tf::Pose> tf_pose_stamped;
     ROS_DEBUG("computing transform from the goal to the base link");
-    try 
+    try
     {
       std::string error_msg;
       ret1 = tf_.waitForTransform(root_name_, goal.pose.header.frame_id, goal.pose.header.stamp,
 				  ros::Duration(5.0), ros::Duration(0.01), &error_msg);
-      // Transforms the pose into the root link frame   
+      // Transforms the pose into the root link frame
       tf::poseStampedMsgToTF(goal.pose, tf_pose_stamped);
       tf_.transformPose(root_name_, tf_pose_stamped, tf_pose_stamped);
     }
@@ -183,7 +169,7 @@ public:
     tf::PoseTFToKDL(tf_pose_stamped, desired_pose);
 
 
-    //get the IK seed 
+    //get the IK seed
     for(int i=0; i < dimension_; i++)
     {
       jnt_pos_suggestion_(getJointIndex(goal.ik_seed.name[i])) = goal.ik_seed.position[i];
@@ -195,12 +181,12 @@ public:
     if(!is_valid)
     {
       ROS_ERROR("%s: Aborted: IK invalid", action_name_.c_str());
-      //set the action state to aborted                                                       
+      //set the action state to aborted
       as_.setAborted();
       return;
     }
 
-    //Now put the jnt commands back in the correct order for the trajectory controller      
+    //Now put the jnt commands back in the correct order for the trajectory controller
     pr2_controllers_msgs::QueryTrajectoryState traj_state;
     traj_state.request.time = ros::Time::now();
     if (!query_traj_srv_.call(traj_state))
@@ -228,19 +214,19 @@ public:
     traj_goal.trajectory.points[1].velocities = velocities;
     ROS_DEBUG("filled out trajectory");
 
-    //unwrap angles   
-    trajectory_unwrap::unwrap(robot_model, traj_goal.trajectory,traj_goal.trajectory);  
+    //unwrap angles
+    trajectory_unwrap::unwrap(robot_model, traj_goal.trajectory,traj_goal.trajectory);
 
-    //Compute the duration of the trajectory                                                    
-    if(goal.move_duration == ros::Duration(0.0)) 
+    //Compute the duration of the trajectory
+    if(goal.move_duration == ros::Duration(0.0))
     {
 	    double dist = 0;
 	    for(int i=0; i < dimension_; i++)
 		    dist = pow(traj_goal.trajectory.points[1].positions[i] - traj_state.response.position[i],2)+dist;
 	    dist = sqrt(dist);
 	    traj_goal.trajectory.points[1].time_from_start = ros::Duration(dist / max_velocity_);
-    } 
-    else 
+    }
+    else
     {
 	    traj_goal.trajectory.points[1].time_from_start = goal.move_duration;
     }
@@ -254,21 +240,21 @@ public:
     if(trajectory_action_->getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
       {
 	ROS_ERROR("%s: Aborted: trajectory action failed to achieve goal", action_name_.c_str());
-	//set the action state to aborted                                                                    
+	//set the action state to aborted
 	as_.setAborted(result_);
         return;
       }
-    
+
     ROS_INFO("%s: Succeeded", action_name_.c_str());
-    // set the action state to succeeded                                                      
+    // set the action state to succeeded
     as_.setSucceeded(result_);
   }
 
 
   int getJointIndex(const std::string &name)
   {
-    int i=0; // segment number                                                                                                                              
-    int j=0; // joint number                                                                                                                                
+    int i=0; // segment number
+    int j=0; // joint number
     while(j < dimension_ && i < (int) kdl_chain_.getNrOfSegments())
     {
       if(kdl_chain_.getSegment(i).getJoint().getType() == KDL::Joint::None)
