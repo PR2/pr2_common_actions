95a4a03fc0d7b35760ef5753def57c4fb1743949
rusu <rusu@7275ad9f-c29b-430a-bdc5-66f4b3af1622>
2010-08-10 03:40:12 +0000

    loop indefinitely in grab result

--- pr2_tilt_laser_interface/src/grab_result.cpp
+++ pr2_tilt_laser_interface/src/grab_result.cpp
@@ -3,21 +3,24 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl/io/pcd_io.h>
 
-int main(int argc, char** argv)
+int 
+  main (int argc, char** argv)
 {
-  ros::init(argc, argv, "result_grabber");
+  ros::init (argc, argv, "result_grabber");
   ros::NodeHandle nh;
   ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("snapshot", 1, true);
-  printf("Waiting for cloud to come in over ros\n");
-  pr2_tilt_laser_interface::GetSnapshotActionResultConstPtr action_result = ros::topic::waitForMessage<pr2_tilt_laser_interface::GetSnapshotActionResult>("get_laser_snapshot/result");
-  sensor_msgs::PointCloud2 cloud = action_result->result.cloud;
 
+  while (nh.ok ())
+  {
+    ROS_INFO ("Waiting for cloud to come in over ROS");
+    pr2_tilt_laser_interface::GetSnapshotActionResultConstPtr action_result = ros::topic::waitForMessage<pr2_tilt_laser_interface::GetSnapshotActionResult>("get_laser_snapshot/result");
+    sensor_msgs::PointCloud2 cloud = action_result->result.cloud;
 
-  pub.publish(cloud);
+    pub.publish (cloud);
+    ROS_INFO ("Published the point cloud!\n");
+    ros::spinOnce ();
+    ros::Duration (0.5).sleep ();
+  }
 
-  printf("Published the point cloud!\n");
-
-  ros::spin();
-
-  return 0;
+  return (0);
 }
